e62f9c101899b77f2c0b823e6eeb882b9a4175bf
Oleksandr <collywolly9798@gmail.com>
2025-04-25 17:10:56 +0200

    first part

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,8 +1,10 @@
 import Text from '../components/Text/Text';
-
+import Form from '../components/Form/Form';
 const Photos = () => {
+  const
   return (
     <>
+      <Form />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,8 +1,38 @@
+import { nanoid } from 'nanoid';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
+import { useEffect, useState } from 'react';
+import TodoList from '../components/TodoList/TodoList';
 
 const Todos = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = window.localStorage.getItem('todos');
+    if (savedTodos !== null) {
+      return JSON.parse(savedTodos);
+    }
+    return [];
+  });
+  const addNewTodo = task => {
+    const newTodo = {
+      id: nanoid(5),
+      text: task,
+    };
+    setTodos(prev => {
+      return [...prev, newTodo];
+    });
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const deleteTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id));
+  };
   return (
     <>
+      <Form onSubmit={addNewTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} />
       <Text textAlign="center">There are no any todos ...</Text>
     </>
   );
